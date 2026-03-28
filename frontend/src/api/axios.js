fa811a581ad59b661f4505565500236cd4382de8
Pratyush-A <163624133+Pratyush-A@users.noreply.github.com>
2026-03-28 22:35:10 +0530

    fixed cors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://notes-app-auth-rbac.onrender.com/api/v1",
+  baseURL: "https://notes-app-auth-rbac-1.onrender.com/api/v1",
   withCredentials: true
 });
 
